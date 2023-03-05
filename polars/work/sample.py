174976ff581e71e38d6488676c64fa4b9a0dcc4b
ankokumoyashi <gen.4048.1105@gmail.com>
2023-03-05 13:53:17 +0900

    parquetのフィルターを測定

--- polars/work/sample.py
+++ polars/work/sample.py
@@ -11,13 +11,17 @@ def main():
     pd_df = pandas_read_parquet(input_parquet)
     #pl_df = polars_read_csv(input_csv)
     pl_df = polars_read_parquet(input_parquet)
+    import ipdb;ipdb.set_trace()
+    pl_df = pl_df.with_columns(pl.col("question").repeat_by(10).alias("question_longer"))
+    import ipdb;ipdb.set_trace()
+    pd_df["question_longer"] = pd_df["question"].apply(lambda x: [x]*10)
 
-    function(pl_df)
+    polars_filter(pl_df)
+    pandas_filter(pd_df)
     with pl.Config() as cfg:
         cfg.set_tbl_cols(-1)  
         cfg.set_tbl_rows(-1)  
         cfg.set_fmt_str_lengths(1000)  
-        print(pl_df)
 
     pd_df = pandas_read_csv_filter_column(input_csv)
     pd_df = pandas_read_parquet_filter_column(input_parquet)
@@ -25,11 +29,12 @@ def main():
     pl_df = polars_read_parquet_filter_column(input_parquet)
 
 @stop_watch
-def function(df):
-    import ipdb;ipdb.set_trace()
-    df = df.select(pl.col("qid").filter(pl.col("answer_entity") == "コックリさん"))
-    #df = df.select([pl.col("qid").sort().head(2), pl.col("answer_entity")])
-    return df
+def polars_filter(df):
+    qid = df.select(pl.col("question_longer").where(pl.col("answer_entity") == "扁平足"))
+
+@stop_watch
+def pandas_filter(df):
+    qid = df[df["answer_entity"] == "扁平足"]["question_longer"]
 
 @stop_watch
 def polars_read_csv(input_path):
@@ -37,7 +42,7 @@ def polars_read_csv(input_path):
 
 @stop_watch
 def polars_read_parquet(input_path):
-    return pl.scan_parquet(input_path)
+    return pl.read_parquet(input_path)
     
 @stop_watch
 def pandas_read_csv(input_path):
